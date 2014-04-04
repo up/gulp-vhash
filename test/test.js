e864fdb53301dd9c061e10715faed9fe9fe355e7
Uli Preuss <me@ulipreuss.eu>
2014-04-04 21:29:41 +0200

    cleanup mocha test file

--- test/test.js
+++ test/test.js
@@ -11,73 +11,72 @@ describe('gulp-vhash tests', function(){
   describe('createHashString()', function(){
 
     it('should return correct hash', function(){
-			
-			var stream = vhash();
+      
+      var stream = vhash();
 
-			stream.on('data', function (file) {
-				//assert.equal(file.orig.path, testfile);
-				assert.equal(file.hash, testhash);
-			});
+      stream.on('data', function (file) {
+        assert.equal(file.hash, testhash);
+      });
 
-			stream.write(new gutil.File({
-				path: testfile,
-				contents: new Buffer('')
-			}));
+      stream.write(new gutil.File({
+        path: testfile,
+        contents: new Buffer('')
+      }));
     
-		});
+    });
   
-	});
+  });
 
   describe('addFileData()', function(){
     it('should create correct json hash object', function(){
-			
-			var stream = vhash();
+      
+      var stream = vhash();
 
-			stream.write(new gutil.File({
-				path: testfile,
-				contents: new Buffer('')
-			}));
+      stream.write(new gutil.File({
+        path: testfile,
+        contents: new Buffer('')
+      }));
     
-			stream.end(function () {
-				vhash.addFileData(testfile, { type : ['css'], path : false}), 
-				assert.equal(
-					vhash.data['test.css'],
-					testhash
-				);
-			});
+      stream.end(function () {
+        vhash.addFileData(testfile, { type : ['css'], path : false}), 
+        assert.equal(
+          vhash.data['test.css'],
+          testhash
+        );
+      });
 
-		});
+    });
   
-	});
+  });
 
   describe('replaceHash()', function(){
 
     it('should creat hash parameter', function(){
-			
-			var stream = vhash();
+      
+      var stream = vhash();
     
-			stream.end(function (file) {
-				assert.equal(vhash.replaceHash(
-					'<link rel="stylesheet" href="assets/stylesheets/common.css?v=1234567890abcde"/>', 
-					testhash, 'common.css'
-				), '<link rel="stylesheet" href="assets/stylesheets/common.css?v=' + testhash + '"/>');
-			});
+      stream.end(function (file) {
+        assert.equal(vhash.replaceHash(
+          '<link rel="stylesheet" href="assets/stylesheets/common.css?v=1234567890abcde"/>', 
+          testhash, 'common.css'
+        ), '<link rel="stylesheet" href="assets/stylesheets/common.css?v=' + testhash + '"/>');
+      });
 
-		});
-		
+    });
+    
     it('should replace hash parameter', function(){
-			
-			var stream = vhash();
+      
+      var stream = vhash();
     
-			stream.end(function (file) {
-				assert.equal(vhash.replaceHash(
-					'<link rel="stylesheet" href="assets/stylesheets/common.css"/>', 
-					testhash, 'common.css'
-				), '<link rel="stylesheet" href="assets/stylesheets/common.css?v=' + testhash + '"/>');
-			});
+      stream.end(function (file) {
+        assert.equal(vhash.replaceHash(
+          '<link rel="stylesheet" href="assets/stylesheets/common.css"/>', 
+          testhash, 'common.css'
+        ), '<link rel="stylesheet" href="assets/stylesheets/common.css?v=' + testhash + '"/>');
+      });
 
-		});
+    });
   
-	});
+  });
 
 });
